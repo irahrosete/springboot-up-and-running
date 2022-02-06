a75dcdf62f9f048c51a7f068ae6f00195a64b50a
irah rosete <irah.rosete@outlook.com>
2022-02-06 21:15:41 +1100

    initial commit

---
+++ src/main/java/com/example/springbootupandrunning/SpringbootUpAndRunningApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootupandrunning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUpAndRunningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUpAndRunningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootupandrunning/SpringbootUpAndRunningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootupandrunning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUpAndRunningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

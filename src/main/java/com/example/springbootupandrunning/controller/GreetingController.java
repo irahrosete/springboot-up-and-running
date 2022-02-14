dce804425d1df50a681618537162f06c1be0cd6c
irah rosete <irah.rosete@outlook.com>
2022-02-14 22:07:54 +1100

    add dependency and refactor GreetingController

--- src/main/java/com/example/springbootupandrunning/controller/GreetingController.java
+++ src/main/java/com/example/springbootupandrunning/controller/GreetingController.java
@@ -1,6 +1,6 @@
 package com.example.springbootupandrunning.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.example.springbootupandrunning.domain.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-    @Value("${greeting-name: Irah}")
-    private String name;
+    private final Greeting greeting;
 
-    @Value(("${greeting-coffee: ${greeting-name} is drinking Cafe Ganador}"))
-    private String coffee;
+    public GreetingController(Greeting greeting) {
+        this.greeting = greeting;
+    }
 
     @GetMapping
     String getGreeting() {
-        return name;
+        return greeting.getName();
     }
 
     @GetMapping("/coffee")
     String getNameAndCoffee() {
-        return coffee;
+        return greeting.getCoffee();
     }
 
 }

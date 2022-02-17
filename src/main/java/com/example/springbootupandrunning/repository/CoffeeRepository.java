3e4fbb0f45ba860ac20ac0fde488ed7673c1beb0
irah rosete <irah.rosete@outlook.com>
2022-02-17 19:10:21 +1100

    create Aircraft domain and add dependencies

--- src/main/java/com/example/springbootupandrunning/repository/CoffeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootupandrunning.repository;
-
-import com.example.springbootupandrunning.domain.Coffee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CoffeeRepository extends CrudRepository<Coffee, String> {
-}

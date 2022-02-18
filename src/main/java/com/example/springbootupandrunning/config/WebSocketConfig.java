e39d553c76da4e07b16123ee1c7e4c69becf3b69
irah rosete <irah.rosete@outlook.com>
2022-02-18 13:21:23 +1100

    create WebSocketConfig

---
+++ src/main/java/com/example/springbootupandrunning/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.example.springbootupandrunning.config;
+
+import com.example.springbootupandrunning.socket.WebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    public WebSocketConfig(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws");
+    }
+}
